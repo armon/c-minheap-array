65db6c9088265309f7552a7e3e2c4c681a00ec3a
Armon Dadgar <armon.dadgar@gmail.com>
2011-04-16 12:57:35 -0700

    Minor cleanup of main file

--- main.c
+++ main.c
@@ -6,17 +6,6 @@
 #include <limits.h>
 #include "heap.h"
 
-
-int index_counter = 0;
-
-void verify_order(void* key, void* value) {
-    // Get the key
-    int val = *(int*)key;
-
-    printf("%d - Key: %d\n",index_counter,val);
-    index_counter++;
-}
-
 int main(int argc, char** argv) {
 
     // Create the heap
@@ -24,14 +13,14 @@ int main(int argc, char** argv) {
     heap_create(&h,0,NULL);
 
     // Maximum
-    int count = 10000000;
+    int count = 10000000; // 10M
 
     // Allocate a key and value
     int* key = (int*)malloc(count*sizeof(int));
     char* value = "The meaning of life.";
 
     // Initialize the first key
-    unsigned int val = 123;
+    unsigned int val = 42;
     srand( val);
     printf("Seed %d\n",val);
 
@@ -51,24 +40,18 @@ int main(int argc, char** argv) {
     }
 
 
-
     // Get the minimum
     int* min_key;
     char* min_val;
-    int *prev_key = NULL;
+    int *prev_key = &min;
 
     // Show the real minimum 
     printf("Real min: %d\n", min);
 
     // Try to get the minimum
     while (heap_delmin(&h, &min_key, &min_val)) {
-        // Dump
-        //index_counter = 0;
-        //heap_foreach(&h, verify_order);
-
-        // printf("Key: %d Val: %s\n",*min_key,min_val);
-
-        if (prev_key != NULL && *prev_key > *min_key) {
+        // Verify that the values are getting larger
+        if (*prev_key > *min_key) {
             printf("Previous key is greater than current key!\n");
         }
         prev_key = min_key;
